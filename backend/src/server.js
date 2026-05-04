384ff56af868e47d15ca91a05d4aa72b8eaa1e4f
rushdy87 <mohammed_alrushdy@yahoo.com>
2026-05-05 01:32:40 +0300

    Connect a db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,27 @@
 import app from './app.js';
 import env from './config/env.js';
+import sequelize from './config/database.js';
 
-const server = app.listen(env.PORT, () => {
-  console.log(`Server running in ${env.NODE_ENV} mode on port ${env.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
 
-process.on('unhandledRejection', (err) => {
-  console.error('Unhandled Rejection:', err.name, err.message);
-  server.close(() => {
+    console.log('Database connected successfully');
+
+    const server = app.listen(env.PORT, () => {
+      console.log(`Server running in ${env.NODE_ENV} mode on port ${env.PORT}`);
+    });
+
+    process.on('unhandledRejection', (err) => {
+      console.error('Unhandled Rejection:', err.name, err.message);
+      server.close(() => {
+        process.exit(1);
+      });
+    });
+  } catch (err) {
+    console.error('Unable to connect to database:', err.message);
     process.exit(1);
-  });
-});
+  }
+};
+
+startServer();
